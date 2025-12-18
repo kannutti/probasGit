f470ecc000661aca6571eb31e976062b6f9ffad7
miguel <kannutti@yahoo.es>
2025-12-18 16:35:53 +0100

    Regex para cambiar enlaces de ficheros antiguos.

--- src/es/gob/educacion/Main.java
+++ src/es/gob/educacion/Main.java
@@ -33,6 +33,17 @@ public class Main {
 		System.out.println("Texto sustituido: " + textoNew);
 		
 		
+		//String prueba = "<p>Esto es una prueba.</p><p><br></p><p><br></p><table style=\"border: 1.0px solid rgb(0,0,0);\"><tbody><tr><td style='border: 1px solid black; padding: 5px;'><strong><em><u>Segunda en negrita</u></em></strong></td></tr></tbody></table><p><br></p><p>Link antiguo: <a href=\"fiscalicexlink:?p=|https://local.tcu.es:8443/webdav2/IDFICHERO_26257/papel5.docx\" rel=\"noopener noreferrer\">[papel5.docx]</a></p>";
+		String prueba = "<p>Link antiguo: <a href=\"fiscalicexlink:?p=|https://local.tcu.es:8443/webdav2/IDFICHERO_26257/papel5.docx\">[papel5.docx]</a></p>";
+		//String regexComplemento = "fiscalicexlink:\\?p=|^https?://([^/]+)/webdav2/IDFICHERO_(\\d+)/(^/)+\\\"";
+		String regexComplemento = "fiscalicexlink:\\?p=\\|https://[a-zA-Z\\.]+:[\\d]+/webdav2/IDFICHERO_(\\d+)/[^\"]*";
+		
+		System.out.println();
+		System.out.println("-------------------------------------------------------------------------------------------");
+		System.out.println("Texto inicial: " + prueba);
+		//System.out.println("Texto Reemplazado: " + prueba.replaceAll(regexComplemento, "javascript:abrirFicheroDesdeEditorHTML($1,178);"));
+		System.out.println("Texto Reemplazado: " + prueba.replaceAll(regexComplemento, "javascript:abrirFicheroDesdeEditorHTML($1,178);"));
+		
 
 	}
 
